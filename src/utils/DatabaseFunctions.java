69f3bd1d885315ede8477f5bf345841a653d7e4d
PABLO CONTE CORRÊA <0414249@feevale.br>
2025-10-22 21:55:48 -0300

    feat: adding database function class

---
+++ src/utils/DatabaseFunctions.java
@@ -0,0 +1,54 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseFunctions {
+    private static final String URL = "jdbc:postgresql://localhost/gabriel";
+    private static final String USER = "gabriel";
+    private static final String PASSWORD = "12345";
+
+    public void verifyGameState(String name, String email) {
+        // just example
+        //     String sql = "INSERT INTO users (name, email) VALUES (?, ?)";
+
+        //     try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        //          PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+        //         stmt.setString(1, name);
+        //         stmt.setString(2, email);
+        //         stmt.executeUpdate();
+        //         System.out.println("User created successfully!");
+
+        //     } catch (SQLException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+        //     String sql = "SELECT id, name, email FROM users";
+        //     List<String> users = new ArrayList<>();
+
+        //     try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        //          Statement stmt = conn.createStatement();
+        //          ResultSet rs = stmt.executeQuery(sql)) {
+
+        //         while (rs.next()) {
+        //             users.add(
+        //                 rs.getInt("id") + " - " +
+        //                 rs.getString("name") + " (" +
+        //                 rs.getString("email") + ")"
+        //             );
+        //         }
+
+        //     } catch (SQLException e) {
+        //         e.printStackTrace();
+        //     }
+
+        //     return users;
+    }
+}
